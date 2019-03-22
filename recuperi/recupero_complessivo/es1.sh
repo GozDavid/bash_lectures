99e7b5767240466a79b0a51c12c09defb633be81
Sara Bertocco <bertocco@oats.inaf.it>
2019-03-22 23:38:38 +0100

    First version

---
+++ recuperi/recupero_complessivo/es1.sh
@@ -0,0 +1,10 @@
+
+echo "Numero di righe del file attivita_inaf.txt:"
+# qui inserire il comando per contare le righe del file
+echo 
+echo "Numero di occorrenze delle parola Universo"
+# qui inserire il comando per contare le occorrenze delle parola Universo
+echo
+# qui inserire il comando per sostituire in tutto il file 
+# 'CMB' con 'cosmic microwave background'
+
